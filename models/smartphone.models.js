bfda8f528172346dd595e4a33a75722c2a388098
Pritam <jaiswalpritam067@gmail.com>
2026-08-11 11:18:47 +0530

    update code

---
+++ models/smartphone.models.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose")
+
+const smartphoneSchema = new mongoose.Schema({
+    brand:{
+        type: String,
+        required: true,
+    },
+    model:{
+        type: String,
+        requird: true,
+    },
+    releaseYear: {
+        type: String,
+        required: true
+    },
+    operatingSystem: {
+        type: String,
+
+    },
+    displaySize: {
+        type: String,
+    },
+    ram: {
+        type: String,
+    },
+    cameraSpaces:{
+        type: Object,
+    },
+    batteryCapacity:{
+        type: String,
+    },
+    connectivity: [{
+        type: String,
+    }],
+    price: {
+        type: Number,
+    },
+    colorsAvailable: [{
+        type: String,
+    }],
+    features: [{
+        type: String,
+    }],
+
+
+}, {timestamps: true})
+
+const Smartphone = mongoose.model("Smartphone", smartphoneSchema)
+
+module.exports = Smartphone

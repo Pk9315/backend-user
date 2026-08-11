383868b2831f5dd8d1ec4b9d8ab81af2ff3fe2a7
Pritam <jaiswalpritam067@gmail.com>
2026-08-11 12:43:25 +0530

    updated code

--- BE1.1HW_2/product.models.js
+++ BE1.1HW_2/product.models.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose")
 
-const productSchema = new mongoose.Schema({})
+const productSchema = new mongoose.Schema(
+    {
+        productImageUrl: String,
+        productName: String,
+        description: String,
+        price: String,
+        isAvailable: Boolean,
+        isOffer: Boolean,
+        features: String,
+        productCompany: String,
+
+    }
+)
 
 const Product = mongoose.model("Product", productSchema)
 

f99cab2d7a0408dbb105487731ec4dfe373139bf
Pritam <jaiswalpritam067@gmail.com>
2026-08-12 14:41:55 +0530

    updated code

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const {initializeDatabase} = require("./db/db.connect")
+const fs = require("fs")
+const movie = require("./models/movie.models")
 
-initializeDatabase()
+const JsonData = fs.readFileSync("movie.json", "utf-8")
+const movieData = json.parse(JsonData)
+function seedData(){
+    try{
+        for(const movieData of moviesData){
+            const newMovie = new Movie({
+                title: movieData.title,
+                releaseYear: movieData.releaseYear,
+                genre: movieData.genre,
+                director: movieData.director,
+                actors: movieData.actors,
+                language: movieData.language,
+                country: movieData.country,
+                rating: movieData.rating,
+                plot: movieData.plot,
+                awards: movieData.awards,
+                posterUrl: movieData.posterUrl,
+                trailerUrl: movieData.trailerUrl
+
+            })
+            newMovie.save()
+        }
+        
+    }
+    catch(error){
+            console.log("error seeding the data", error)
+        }
+}
+seedData()

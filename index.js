647acb352538abae930c918d88ab9ff6a03fbb09
Pritam <jaiswalpritam067@gmail.com>
2026-08-22 02:39:58 +0530

    updated code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const newMovie =  {
     title: "karan arjun",
     releaseYear: 1995,
     genre: ["Drama"],
-    director: "Aditya Chopra",
+    director: "Aditya Kumar Chopra",
     actors: ["Salman khan", "Kajol"],
     language: "Hindi",
     country: "India",
@@ -22,6 +22,7 @@ async function createdMovie(newMovie) {
     try{
         const movie = new Movie(newMovie)
         const saveMovie = await movie.save()
+        console.log("New movie data: ", saveMovie)
     }
     catch(error){
         throw error

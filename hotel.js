e4c01a5b0a77f22564bace8b1756bd82645190f1
Pritam <jaiswalpritam067@gmail.com>
2026-08-25 17:09:28 +0530

    updated code

--- hotel.js
+++ hotel.js
@@ -24,13 +24,145 @@ const newHotel = {
   photos: ["https://example.com/hotel-photo1.jpg", "https://example.com/hotel-photo2.jpg"],
 };
 
-async function findHotel(newHotel){
+const lakeViewHotel = {
+  name: "Lake View",
+  category: "Mid-Range",
+  location: "124 Main Street, Anytown",
+  rating: 3.2,
+  reviews: [],
+  website: "https://lake-view-example.com",
+  phoneNumber: "+1234555890",
+  checkInTime: "2:00 PM",
+  checkOutTime: "12:00 PM",
+  amenities: ["Laundry", "Boating"],
+  priceRange: "$$$ (31-60)",
+  reservationsNeeded: true,
+  isParkingAvailable: false,
+  isWifiAvailable: true,
+  isPoolAvailable: false,
+  isSpaAvailable: false,
+  isRestaurantAvailable: false,
+  photos: ["https://example.com/hotel1-photo1.jpg", "https://example.com/hotel1-photo2.jpg"],
+};
+const sunsetResort = {
+  name: "Sunset Resort",
+  category: "Resort",
+  location: "12 Main Road, Anytown",
+  rating: 4.0,
+  reviews: [],
+  website: "https://sunset-example.com",
+  phoneNumber: "+1299655890",
+  checkInTime: "2:00 PM",
+  checkOutTime: "11:00 AM",
+  amenities: ["Room Service", "Horse riding", "Boating", "Kids Play Area", "Bar"],
+  priceRange: "$$$$ (61+)",
+  reservationsNeeded: true,
+  isParkingAvailable: true,
+  isWifiAvailable: true,
+  isPoolAvailable: true,
+  isSpaAvailable: true,
+  isRestaurantAvailable: true,
+  photos: ["https://example.com/hotel2-photo1.jpg", "https://example.com/hotel2-photo2.jpg"],
+};
+async function findHotel(newHotels){
     try{
-        const hotel = new Hotel(newHotel)
+        const hotel = new Hotel(newHotels)
         const saveHotelData = await hotel.save()
     }
     catch(error){
         throw error
     }
 }
-findHotel(newHotel)
+// findHotel(newHotel)
+// findHotel(lakeViewHotel)
+// findHotel(sunsetResort)
+
+async function readAllHotel(){
+    try{
+        const readAllHotel = await Hotel.find()
+        console.log(readAllHotel)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// readAllHotel()
+
+async function readHotelByName(hotel){
+    try{
+        const readHotel = await Hotel.findOne({name: hotel})
+        console.log(readHotel)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// readHotelByName("Lake View")
+
+async function readAllParking(){
+    try{
+        const readParking = await Hotel.find({isParkingAvailable:true})
+        console.log(readParking)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// readAllParking()
+
+async function hasResturantInHotel(){
+    try{
+        const hasResturant = await Hotel.find({isRestaurantAvailable: true})
+        console.log(hasResturant)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// hasResturantInHotel()
+
+async function hotelWithMidRange(){
+    try{
+        const hotelMidRange = await Hotel.find({category: "Mid-Range"})
+        console.log(hotelMidRange)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// hotelWithMidRange()
+
+async function hotelPriceRange(){
+    try{
+        const hotelRange = await Hotel.find({priceRange: "$$$$ (61+)"})
+        console.log(hotelRange)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// hotelPriceRange()
+
+
+async function hotelWithRating(){
+    try{
+        const hotelRating = await Hotel.find({rating: 4.0 })
+        console.log(hotelRating)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// hotelWithRating()
+
+async function readHotelByPhoneNumber(number){
+    try{
+        const hotelPhoneNum = await Hotel.findOne({phoneNumber: number})
+        console.log(hotelPhoneNum)
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+readHotelByPhoneNumber("+1299655890")
